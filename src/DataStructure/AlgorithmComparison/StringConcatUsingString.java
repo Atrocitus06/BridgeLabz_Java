d163e4878830d7c8ec3c7822d6dcb7c13a44666b
Atrocitus06 <anshulbagchi06@gmail.com>
2026-01-24 10:24:31 +0530

    [Anshul] : [Added] : Added comparisons of each of the algorithms

---
+++ src/DataStructure/AlgorithmComparison/StringConcatUsingString.java
@@ -0,0 +1,14 @@
+package DataStructure.AlgorithmComparison;
+public class StringConcatUsingString {
+
+    public static void main(String[] args) {
+
+        String str = "";
+
+        for (int i = 0; i < 1000; i++) {
+            str = str + "a";
+        }
+
+        System.out.println("Final String Length: " + str.length());
+    }
+}
---
+++ src/DataStructure/AlgorithmComparison/StringConcatUsingStringBuilder.java
@@ -0,0 +1,14 @@
+package DataStructure.AlgorithmComparison;
+public class StringConcatUsingStringBuilder {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 1000; i++) {
+            sb.append("a");
+        }
+
+        System.out.println("Final String Length: " + sb.length());
+    }
+}
